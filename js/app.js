1cf266233e65df695f226d6abf63e9da09adbe26
HishamAl-Naji <hishamnaji1010@gmail.com>
2020-06-10 01:46:43 +0300

    resubmit lab02

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
-'use strict'
+'use strict;';
+
+
+// User welcoming :
+
+alert('We need to know about you :) ');
+var UN ;
+
+function userName() {
+
+    UN = prompt('What is your name please ');
+
+    alert('Wlecome to our website ' + UN);
+
+    return UN;
+}
+userName();
 
 // User's 5 Questions  :
-var rightA = 0 ;
+var rightA = 0;
 
 var userQ1 = '';
 var userQ2 = '';
@@ -9,26 +25,36 @@ var userQ3 = '';
 var userQ4 = '';
 var userQ5 = '';
 
-while (userQ1 != 'yes' && userQ1 != 'no') {
+while ((userQ1 != 'yes' && userQ1 != 'y') && (userQ1 != 'no'&& userQ1!= 'n')) {
 
-    userQ1 = prompt('do you think i use sarcasm ?');
+    userQ1 = prompt('do you think i use sarcasm ? yes/no');
     userQ1 = userQ1.toLowerCase();
     if (userQ1 == 'yes') {
         alert("Right answer");
         rightA++;
     } else if (userQ1 == 'no') {
         alert("wrong answer")
+    }else if (userQ1 == 'y') {
+        alert("right answer")
+        rightA++;
+    }else if (userQ1 == 'n') {
+        alert("wrong answer")
     }
 }
 // console.log('do you think i use sarcasm ?' + userQ1);
 
-while (userQ2 != 'yes' && userQ2 != 'no') {
+while ((userQ2 != 'yes' && userQ2 != 'y') && (userQ2 != 'no'&& userQ2!= 'n')) {
 
-    userQ2 = prompt('do you think i am heavy ?');
+    userQ2 = prompt('do you think i am heavy ? yes/no');
     userQ2 = userQ2.toLowerCase();
     if (userQ2 == 'yes') {
-        alert("wrong answer");
+        alert("wrong answer")
     } else if (userQ2 == 'no') {
+        alert("right answer")
+        rightA++;
+    }else if (userQ2 == 'y') {
+        alert("wrong answer")
+    }else if (userQ2 == 'n') {
         alert("right answer")
         rightA++;
     }
@@ -36,51 +62,68 @@ while (userQ2 != 'yes' && userQ2 != 'no') {
 // console.log('do you think i am heavy ?' + userQ1);
 
 
-while (userQ3 != 'yes' && userQ3 != 'no') {
+while ((userQ3 != 'yes' && userQ3 != 'y') && (userQ3 != 'no'&& userQ3!= 'n')) {
 
-    userQ3 = prompt('do you think i can swim ?');
+    userQ3 = prompt('do you think i can swim ? yes/no');
     userQ3 = userQ3.toLowerCase();
     if (userQ3 == 'yes') {
         alert("Right answer");
         rightA++;
     } else if (userQ3 == 'no') {
         alert("wrong answer")
+    }else if (userQ3 == 'y') {
+        alert("right answer")
+        rightA++;
+    }else if (userQ3 == 'n') {
+        alert("wrong answer")
     }
 }
 // console.log('do you think i can swim ?' + userQ1);
 
-while (userQ4 != 'yes' && userQ4 != 'no') {
 
-    userQ4 = prompt('do you think that i am Committed to my religion ?');
+while ((userQ4 != 'yes' && userQ4 != 'y') && (userQ4 != 'no'&& userQ4!= 'n')) {
+
+    userQ4 = prompt('do you think that i am Committed to my religion ?yes/no');
     userQ4 = userQ4.toLowerCase();
     if (userQ4 == 'yes') {
         alert("Right answer");
         rightA++;
     } else if (userQ4 == 'no') {
         alert("wrong answer")
+    }else if (userQ4 == 'y') {
+        alert("right answer")
+        rightA++;
+    }else if (userQ4 == 'n') {
+        alert("wrong answer")
     }
 }
 // console.log('do you think that i am Committed to my religion ?' + userQ1);
 
-while (userQ5 != 'yes' && userQ5 != 'no') {
+while ((userQ5 != 'yes' && userQ5 != 'y') && (userQ5 != 'no'&& userQ5!= 'n')) {
 
-    userQ5 = prompt('do you think i watch football ?');
+    userQ5 = prompt('do you think i watch football ?yes/no');
     userQ5 = userQ5.toLowerCase();
     if (userQ5 == 'yes') {
         alert("Right answer");
         rightA++;
     } else if (userQ5 == 'no') {
         alert("wrong answer")
+    }else if (userQ5 == 'y') {
+        alert("right answer")
+        rightA++;
+    }else if (userQ5 == 'n') {
+        alert("wrong answer")
     }
 }
+
 // console.log('do you think i watch football ?' + userQ1);
 
 
 
 // Qestion #6
-var userQ6 ;
+var userQ6;
 var i = 0;
-while (userQ6 != 11 && i<4) {
+while (userQ6 != 11 && i < 4) {
 
     userQ6 = prompt('Pick a number between 1-15'); // the number is 11 :)
     if (userQ6 == 11) {
@@ -88,7 +131,7 @@ while (userQ6 != 11 && i<4) {
         rightA++;
     } else if (userQ6 < 11) {
         alert("Too low");
-    }else if (userQ6 > 11) {
+    } else if (userQ6 > 11) {
         alert("Too high");
     }
     i++;
@@ -123,24 +166,13 @@ while (!correctA && i < 6) {
     if (i == 6) {
         alert('Sorry you have been exhausted all attempts and the countries are : saudia, uae and syria')
     }
-} 
+}
+
+// # of right questions :)
 
 if (rightA >= 4) {
     alert('Great , You answered ' + rightA + ' questions correct !')
-}else{
+} else {
     alert('You just answered ' + rightA + ' questions correct')
 }
 // console.log('the user pick' + userQ7);
-
-// User welcoming :
-
-alert('Now we need to know about you :) ')
-
-function userName() {
-
-    var UN = prompt('What is your name please ')
-
-    alert('Wlecome to our website ' + UN)
-
-    return UN;
-}
