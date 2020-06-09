58861b4ea827172d5583b671e8cb2ede14f5bd61
HishamAl-Naji <hishamnaji1010@gmail.com>
2020-06-09 17:41:58 +0300

    instructios are ready

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict'
 
 // User's 5 Questions  :
+var rightA = 0 ;
 
 var userQ1 = '';
 var userQ2 = '';
@@ -14,6 +15,7 @@ while (userQ1 != 'yes' && userQ1 != 'no') {
     userQ1 = userQ1.toLowerCase();
     if (userQ1 == 'yes') {
         alert("Right answer");
+        rightA++;
     } else if (userQ1 == 'no') {
         alert("wrong answer")
     }
@@ -28,6 +30,7 @@ while (userQ2 != 'yes' && userQ2 != 'no') {
         alert("wrong answer");
     } else if (userQ2 == 'no') {
         alert("right answer")
+        rightA++;
     }
 }
 // console.log('do you think i am heavy ?' + userQ1);
@@ -39,6 +42,7 @@ while (userQ3 != 'yes' && userQ3 != 'no') {
     userQ3 = userQ3.toLowerCase();
     if (userQ3 == 'yes') {
         alert("Right answer");
+        rightA++;
     } else if (userQ3 == 'no') {
         alert("wrong answer")
     }
@@ -51,6 +55,7 @@ while (userQ4 != 'yes' && userQ4 != 'no') {
     userQ4 = userQ4.toLowerCase();
     if (userQ4 == 'yes') {
         alert("Right answer");
+        rightA++;
     } else if (userQ4 == 'no') {
         alert("wrong answer")
     }
@@ -63,12 +68,69 @@ while (userQ5 != 'yes' && userQ5 != 'no') {
     userQ5 = userQ5.toLowerCase();
     if (userQ5 == 'yes') {
         alert("Right answer");
+        rightA++;
     } else if (userQ5 == 'no') {
         alert("wrong answer")
     }
 }
 // console.log('do you think i watch football ?' + userQ1);
 
+
+
+// Qestion #6
+var userQ6 ;
+var i = 0;
+while (userQ6 != 11 && i<4) {
+
+    userQ6 = prompt('Pick a number between 1-15'); // the number is 11 :)
+    if (userQ6 == 11) {
+        alert("Thats RIGHT");
+        rightA++;
+    } else if (userQ6 < 11) {
+        alert("Too low");
+    }else if (userQ6 > 11) {
+        alert("Too high");
+    }
+    i++;
+    if (i == 4) {
+        alert('Sorry you have been exhausted all attempts and the right number is 11')
+    }
+}
+// console.log('The user pick' + userQ6);
+
+// Qestion #7
+var country = ['saudia', 'syria', 'uae']
+var userQ7;
+var correctA = false;
+var x;
+var i = 0;
+var z = 5;
+while (!correctA && i < 6) {
+    userQ7 = prompt('Pick from these countries that I may have visited before : Saudia , Turkey , Syria , Uae , Spain , Germany , Iraq , Palestine , Egypt , Usa , Qatar');
+    userQ7 = userQ7.toLowerCase();
+    for (var x = 0; x < 3; x++) {
+        if (userQ7 == country[x]) {
+            correctA = true;
+            alert('Thats right')
+            rightA++;
+        }
+    }
+    if (!correctA) {
+        alert('Worng but you still have ' + z + ' attempts')
+    }
+    z--
+    i++
+    if (i == 6) {
+        alert('Sorry you have been exhausted all attempts and the countries are : saudia, uae and syria')
+    }
+} 
+
+if (rightA >= 4) {
+    alert('Great , You answered ' + rightA + ' questions correct !')
+}else{
+    alert('You just answered ' + rightA + ' questions correct')
+}
+// console.log('the user pick' + userQ7);
 
 // User welcoming :
 
