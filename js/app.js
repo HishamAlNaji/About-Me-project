1da7f1a57f86e17be51b779db58eb4da0bc631f6
HishamAl-Naji <hishamnaji1010@gmail.com>
2020-06-11 09:31:23 +0300

    ready

--- js/app.js
+++ js/app.js
@@ -10,13 +10,13 @@ var UN ;
 userName();
 
 //Geussing Game Questions
-q1();
-q2();
-q3();
-q4();
-q5();
-q6();
-q7();
+// q1();
+// q2();
+// q3();
+// q4();
+// q5();
+// q6();
+// q7();
 
 
 function userName() {
